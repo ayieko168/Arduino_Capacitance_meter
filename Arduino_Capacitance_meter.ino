c086e02414bc8bb6edfead2f3c1f3492b4666fe7
Antony Alen <antonyalen1960@gmail.com>
2018-12-09 14:46:33 +0300

    This second vrsion only supports the 1 pF to 1 nF capacitance range

--- Arduino_Capacitance_meter.ino
+++ Arduino_Capacitance_meter.ino
@@ -1,60 +1,84 @@
 #include <Wire.h>
 
-#define analogPin      0
-#define chargePin      13
-#define dischargePin   8
-#define resistorValue  10000.0F  //Remember, we've used a 10K resistor to charge the capacitor
+const int OUT_PIN = A2;
+const int IN_PIN = A0;
+const float IN_STRAY_CAP_TO_GND = 24.48;
+const float IN_CAP_TO_GND  = IN_STRAY_CAP_TO_GND;
+const float R_PULLUP = 34.8;  
+const int MAX_ADC_VALUE = 1023;
 
-unsigned long startTime;
-unsigned long elapsedTime;
-float microFarads;
-float nanoFarads;
-
-void setup() {
-
+void setup()
+{ 
   Serial.begin(9600);
   
-  pinMode(chargePin, OUTPUT);
-  digitalWrite(chargePin, LOW);
-}
-
-void loop() {
-  digitalWrite(chargePin, HIGH);
-  startTime = micros();
-  while (analogRead(analogPin) < 648) {
+  pinMode(OUT_PIN, OUTPUT);
+  pinMode(IN_PIN, OUTPUT);
   }
 
-  elapsedTime = micros() - startTime;
-  microFarads = ((float)elapsedTime / resistorValue) ;
+void loop()
+{
+    pinMode(IN_PIN, INPUT);
+    digitalWrite(OUT_PIN, HIGH);
+    int val = analogRead(IN_PIN);
+    digitalWrite(OUT_PIN, LOW);
 
+    if (val < 1000)
+    {
+      pinMode(IN_PIN, OUTPUT);
 
+      float capacitance = (float)val * IN_CAP_TO_GND / (float)(MAX_ADC_VALUE - 
 
-  if (microFarads > 1) {
+val);
 
-    Serial.print("SCALE:  0.1uF-4F  ");
-    Serial.print(microFarads / 2.0);
-    Serial.print(" uF");
-    delay(500);
+      Serial.print("Scale:   1pF-1nF  ");
+      Serial.print(capacitance,3);
+      Serial.println(" pF");
+    }
+    
+    else
+    {
+      pinMode(IN_PIN, OUTPUT);
+      delay(1);
+      pinMode(OUT_PIN, INPUT_PULLUP);
+      unsigned long u1 = micros();
+      unsigned long t;
+      int digVal;
+
+      do
+      {
+        digVal = digitalRead(OUT_PIN);
+        unsigned long u2 = micros();
+        t = u2 > u1 ? u2 - u1 : u1 - u2;
+      } 
+      
+      while ((digVal < 1) && (t < 400000L));
+
+      pinMode(OUT_PIN, INPUT);  
+      val = analogRead(OUT_PIN);
+      digitalWrite(IN_PIN, HIGH);
+      int dischargeTime = (int)(t / 1000L) * 5;
+      delay(dischargeTime);   
+      pinMode(OUT_PIN, OUTPUT);  
+      digitalWrite(OUT_PIN, LOW);
+      digitalWrite(IN_PIN, LOW);
+
+      float capacitance = -(float)t / R_PULLUP / log(1.0 - (float)val / (float)MAX_ADC_VALUE);
+
+      Serial.print("Scale:   1pF-1nF  ");
+      
+      if (capacitance > 1000.0)
+      {
+        Serial.print(capacitance / 1000.0, 2);
+        Serial.println(" uF ");
+        
+      }
+        
+      else
+      {
+        Serial.print(capacitance);
+        Serial.println(" nF");
+        
+      }
   }
-
-  else {
-    nanoFarads = microFarads * 1000.0;
-    Serial.print("SCALE:  0.1uF-4F  ");
-    Serial.print(nanoFarads / 2.0);
-    Serial.print(" nF");
-    delay(500);
-  }
-
-
-  digitalWrite(chargePin, LOW);
-  pinMode(dischargePin, OUTPUT);
-  digitalWrite(dischargePin, LOW);     //discharging the capacitor
-  while (analogRead(analogPin) > 0) {
-  }//This while waits till the capaccitor is discharged
-
-  pinMode(dischargePin, INPUT);      //this sets the pin to high impedance
-
-  Serial.println(" ");
-
-
+    while (micros() % 1000 != 0);
 }
